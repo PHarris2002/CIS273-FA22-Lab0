822d04b0820d41bd82a28cd9573ccb3a93842bf4
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2022-09-03 23:27:25 -0500

    Merge Arrays Complete 9/3 11:27pm

--- MergeArrays/Program.cs
+++ MergeArrays/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace MergeArrays
+﻿using System.ComponentModel;
+
+namespace MergeArrays
 {
 
     public class Program
@@ -10,7 +12,27 @@
 
         public static int[] MergeSortedArrays(int[] array1, int[] array2)
         {
-            return new int[10];
+            //creates an empty list for adding numbers from arrays and for later flexible modification
+            List<int> combinedArrayList = new List<int>();
+
+            //adds all numbers from both arrays to the combinedArrayList
+            foreach (int number in array1)
+            {
+                combinedArrayList.Add(number);
+            }
+
+            foreach (int number in array2)
+            {
+                combinedArrayList.Add(number);
+            }
+
+            //sorts all numbers in the now occupied combinedArrayList
+            combinedArrayList.Sort();
+
+            //converts the list into an array
+            int[] combinedArray = combinedArrayList.ToArray();
+
+            return combinedArray;
         }
     }
 
