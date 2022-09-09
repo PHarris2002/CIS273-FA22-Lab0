0d18cc7b78fb431b7298034c7ddbc0a27769d800
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2022-09-08 21:45:47 -0500

    test

--- Uno/Player.cs
+++ Uno/Player.cs
@@ -3,32 +3,9 @@ namespace Uno
 {
 	public class Player
 	{
-		public string Name { get; set; }
-        public List<Card> Hand { get; set; }
-
-
-        public Player()
+		public Player()
 		{
-			Name = "";
-			Hand = new List<Card>();
 		}
-
-        public bool HasPlayableCard(Card card)
-		{
-			return false;
-		}
-
-        public Card GetFirstPlayableCard(Card card)
-		{
-			return new Card();
-		}
-
-        public Color MostCommonColor()
-		{
-			return Color.Blue;
-		}
-
-
-    }
+	}
 }
 
